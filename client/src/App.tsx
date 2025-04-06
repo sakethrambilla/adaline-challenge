55303032935170836091522cb1e8b57c8d92bcfd
sakethrambilla <sakethrambilla@gmail.com>
2025-04-06 23:59:23 +0530

    chore(client): project setup with shadcn ui and zustand state management

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+
+function App() {
+
+  return (
+    
+     
+          <div className="flex h-screen w-screens bg-blue-400 items-center justify-center">
+            <Button>Click me</Button>
+          </div>
+
+  )
+}
+
+export default App
